05832646a2f6d3ecae96aa5d51650a85fd17f08f
Fan Yang <yangf6@uw.edu>
2016-05-12 23:39:07 -0700

    Update multiChart.js

--- js/multiChart.js
+++ js/multiChart.js
@@ -21,51 +21,46 @@ function multiChart() {
   function chart(selection) {
     selection.each(function(data) {
 
-      // Convert data to standard representation greedily;
-      // this is needed for nondeterministic accessors.
       data = data.map(function(d, i) {
         return [xValue.call(data, d, i), yValue.call(data, d, i)];
       });
 
-      // Update the x-scale.
       xScale
           .domain(d3.extent(data, function(d) { return d[0]; }))
           .range([0, width - margin.left - margin.right]);
 
-      // Update the y-scale.
       yScale
           .domain([0, d3.max(data, function(d) { return d[1]; })])
           .range([height - margin.top - margin.bottom, 0]);
 
-      // Select the svg element, if it exists.
+
       var svg = d3.select(this).selectAll("svg").data([data]);
 
-      // Otherwise, create the skeletal chart.
+
       var mainChart = svg.enter().append("svg").append("g");
       mainChart.append("path").attr("class", "area");
       mainChart.append("path").attr("class", "line");
       mainChart.append("g").attr("class", "x axis");
       mainChart.append("g").attr("class", "y axis");
 
-      // Update the outer dimensions.
       svg .attr("width", width)
           .attr("height", height);
 
-      // Update the inner dimensions.
+
       var g = svg.select("g")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      // Update the area path.
+
      g.select(".area")
           .attr("d", area.y0(yScale.range()[0]))
           .attr("fill", areaColor);
 
-      // Update the line path.
+
       g.select(".line")
           .attr("d", line)
           .attr("stroke", lineColor);
 
-      // Update the x-axis.
+
       g.select(".x.axis")
           .attr("transform", "translate(0," + yScale.range()[0] + ")")
           .call(xAxis)
@@ -82,12 +77,11 @@ function multiChart() {
     });
   }
 
-  // The x-accessor for the path generator; xScale ∘ xValue.
   function X(d) {
     return xScale(d[0]);
   }
 
-  // The x-accessor for the path generator; yScale ∘ yValue.
+
   function Y(d) {
     return yScale(d[1]);
   }
